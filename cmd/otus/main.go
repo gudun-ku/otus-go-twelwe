7e3a060337015c51885103bfcd97b3a12d7ca1ac
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2020-02-12 21:27:56 +0300

    Factor 9 - add logs

--- cmd/otus/main.go
+++ cmd/otus/main.go
@@ -15,11 +15,14 @@ import (
 )
 
 func main() {
-	logrus.Info("Hello World")
+	log := logrus.New()
+	log.SetOutput(os.Stdout)
+
+	log.Info("Hello world")
 
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
-		logrus.Fatal("Port is not set")
+		log.Fatal("Port is not set")
 	}
 
 	router := mux.NewRouter()
@@ -32,13 +35,19 @@ func main() {
 		Handler: router,
 	}
 
+	log.info("App is starting...")
 	go serv.ListenAndServe()
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	<-interrupt
+	log.Info("Stopping app... ")
 	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelFunc()
-	serv.Shutdown(timeout)
+	err := serv.Shutdown(timeout)
+	if err != nil {
+		log.Error("Error when shutdown app: %v", err)
+	}
+	log.Info("The app is stopped.")
 }
